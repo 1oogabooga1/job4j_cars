47f70ea8a1dea523f8afa40134b284997984e88b
Dmitrii <dmitrijdobrydin@gmail.com>
2025-11-19 18:05:13 +0300

    2. HQL [#6874]

---
+++ db/scripts/003_dml_insert_auto_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO auto_user(login, password) VALUES ('Ivanov', 'root');
+INSERT INTO auto_user(login, password) VALUES ('Petro', 'root');
+INSERT INTO auto_user(login, password) VALUES ('Sidorov', 'root');
---
+++ src/main/java/ru/job4j/repository/UserRepository.java
@@ -0,0 +1,83 @@
+package ru.job4j.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import ru.job4j.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+
+    private final SessionFactory sessionFactory;
+
+    public User create(User user) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return user;
+    }
+
+    public void update(User user) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("UPDATE User SET login = :login, password = :password WHERE id = :id")
+                    .setParameter("login", user.getLogin())
+                    .setParameter("password", user.getPassword())
+                    .setParameter("id", user.getId())
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    public void delete(Integer userId) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE User WHERE id = :id")
+                    .setParameter("id", userId)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    public List<User> findAllOrderById() {
+        Session session = sessionFactory.openSession();
+        Query<User> query = session.createQuery("FROM User", User.class);
+        return query.list();
+    }
+
+    public Optional<User> findById(Integer userId) {
+        Session session = sessionFactory.openSession();
+        Query<User> query = session.createQuery("FROM User u WHERE u.id = :id", User.class)
+                        .setParameter("id", userId);
+        return Optional.ofNullable(query.uniqueResult());
+    }
+
+    public List<User> findByLikeLogin(String key) {
+        Session session = sessionFactory.openSession();
+        Query<User> query = session.createQuery("FROM User u WHERE u.login LIKE :key", User.class)
+                        .setParameter("key", "%" + key + "%");
+        return query.list();
+    }
+
+    public List<User> findByLogin(String login) {
+        Session session = sessionFactory.openSession();
+        Query<User> query = session.createQuery("FROM User u WHERE u.login = :login", User.class)
+                        .setParameter("login", login);
+        return query.list();
+    }
+}
